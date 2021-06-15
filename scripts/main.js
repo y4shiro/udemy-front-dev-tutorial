5697dc2ad5db00e6eb5e9130e0e81c8ef99cb868
y4shiro <y4shironao@gmail.com>
2021-06-15 15:55:19 +0900

    Header の装飾とモバイルメニューの追加

--- scripts/main.js
+++ scripts/main.js
@@ -20,4 +20,19 @@ document.addEventListener('DOMContentLoaded', function () {
   };
 
   const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
+
+  const header = document.querySelector('.header');
+  const _navAnimation = (el, inview) => {
+    if (inview) {
+      header.classList.remove('triggerd');
+    } else {
+      header.classList.add('triggerd');
+    }
+  };
+
+  const so3 = new ScrollObserver('.nav-trigger', _navAnimation, {
+    once: false,
+  });
+
+  new MobileMenu();
 });
